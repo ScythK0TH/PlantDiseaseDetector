b62d9216323b37ab663c81972b6a53ad1e798914
Thunwarat <thunwarat.1812@gmail.com>
2025-03-14 15:42:56 +0100

    detail page + storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'storage_page.dart';
+import 'details_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => StoragePage(),
+        '/details': (context) => DetailsPage(),
+      },
     );
   }
 }
---
+++ lib/storage_page.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+class StoragePage extends StatefulWidget {
+  @override
+  _StoragePageState createState() => _StoragePageState();
+}
+
+class _StoragePageState extends State<StoragePage> {
+  String? _selectedCategory;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(15.0),
+          ),
+        ),
+        backgroundColor: Color(0xFF464646),
+        title: Text(
+          'Storage',
+          style: TextStyle(color: Color(0xFFFFFFFF)),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings, color: Color(0xFFFFFFFF)),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Row(
+              children: [
+                SizedBox(width: 8.0),
+                DropdownButton<String>(
+                  value: _selectedCategory,
+                  items: <String>['ทั้งหมด', 'สุขภาพดี', 'เป็นโรค']
+                      .map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedCategory = newValue;
+                    });
+                  },
+                  hint: Text('ทั้งหมด'),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            title: Text('Plant01'),
+            subtitle: Text('Healthy'),
+            trailing: Icon(Icons.image),
+            onTap: () {
+              Navigator.pushNamed(context, '/details',
+                  arguments: {'title': 'Plant01', 'subtitle': 'healthy'});
+            },
+          ),
+          ListTile(
+            title: Text('Plant02'),
+            subtitle: Text('Healthy'),
+            trailing: Icon(Icons.image),
+            onTap: () {
+              Navigator.pushNamed(context, '/details',
+                  arguments: {'title': 'Plant02', 'subtitle': 'healthy'});
+            },
+          ),
+          ListTile(
+            title: Text('Plant03'),
+            subtitle: Text('Powdery mildew'),
+            trailing: Icon(Icons.image),
+            onTap: () {
+              Navigator.pushNamed(context, '/details', arguments: {
+                'title': 'Plant03',
+                'subtitle': 'Powdery mildew'
+              });
+            },
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Handle add new image
+        },
+        shape: CircleBorder(),
+        backgroundColor: Color(0xFF00BA18),
+        child: Icon(Icons.add, color: Color(0xFFFFFFFF)),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(15.0),
+        ),
+        child: BottomAppBar(
+          color: Color(0xFF464646),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                icon: Icon(Icons.sort, color: Colors.white),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.search, color: Colors.white),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
