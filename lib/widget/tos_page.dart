eabaae03ab2d6d3076fbd80990a5d56bb1ed4360
Yanapatt Pankaseam <prarichatt70@gmail.com>
2025-06-03 14:36:08 +0700

    V3.15: (Term of Service Page) Improve ui design

--- lib/widget/tos_page.dart
+++ lib/widget/tos_page.dart
@@ -1,6 +1,5 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:project_pdd/style.dart';
+import 'package:project_pdd/ui/styles.dart';
 
 class TermOfServicePage extends StatefulWidget {
   const TermOfServicePage({super.key});
@@ -12,14 +11,11 @@ class TermOfServicePage extends StatefulWidget {
 class TermOfServicePageState extends State<TermOfServicePage> {
   @override
   Widget build(BuildContext context) {
-    final double screenWidth = MediaQuery.of(context).size.width;
     final double keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
     return Container(
       alignment: Alignment.center,
       decoration: BoxDecoration(
-        color: Theme.of(context).brightness == Brightness.dark
-            ? primaryColor
-            : Colors.white,
+        color: AppTheme.themedBgColor(context),
         borderRadius: BorderRadius.vertical(top: Radius.circular(36.0)),
       ),
       child: SingleChildScrollView(
@@ -37,17 +33,18 @@ class TermOfServicePageState extends State<TermOfServicePage> {
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    SizedBox(height: 12,),
+                    SizedBox(
+                      height: 12,
+                    ),
                     Text(
                       "Term of Service",
-                      style: subTitleTextStyleDark(context,
-                          fontWeight: FontWeight.bold),
+                      style:
+                          AppTheme.mediumTitle(context, color: AppTheme.dark),
                     ),
                     SizedBox(height: 10),
                     Text(
                       "ข้อตกลงการให้บริการ",
-                      style: descTextStyleDark(context,
-                          fontWeight: FontWeight.normal),
+                      style: AppTheme.smallTitle(context, color: AppTheme.dark),
                     ),
                     SizedBox(height: 12),
                   ],
@@ -61,16 +58,15 @@ class TermOfServicePageState extends State<TermOfServicePage> {
                   Padding(
                     padding: const EdgeInsets.only(left: 32.0, right: 32.0),
                     child: Text.rich(
-                      TextSpan(
-                        children: 
-                        <TextSpan>[
-                          TextSpan(text: '''\u00A0\u00A0\u00A0\u00A0\u00A0'''),
-                          TextSpan(text: '''ซอฟต์แวร์นี้เป็นผลงานที่พัฒนาขึ้นโดย นายพชรดนัย กุระกนก นายญาณภัทร ปานเกษม และ นายธัญวรัตม์ ก.วิบูลย์ศักดิ​ศรี จาก มหาวิทยาลัยศรีนครินทรวิโรฒ ภายใต้การดูแลของ นางสาวบรรพตรี คมขำ ภายใต้โครงการ ใบรู้โรค ซึ่งสนับสนุนโดย สำนักงานพัฒนาวิทยาศาสตร์และเทคโนโลยีแห่งชาติ โดยมีวัตถุประสงค์เพื่อส่งเสริมให้นักเรียนและนักศึกษาได้เรียนรู้และฝึกทักษะในการพัฒนา ซอฟต์แวร์ ลิขสิทธิ์ของซอฟต์แวร์นี้จึงเป็นของผู้พัฒนา ซึ่งผู้พัฒนาได้อนุญาติให้สำนักงาน พัฒนาวิทยาศาสตร์และเทคโนโลยีแห่งชาติ เผยแพร่ซอฟต์แวร์นี้ตาม “ต้นฉบับ” โดยไม่มี การแก้ไขดัดแปลงใด ๆ ทั้งสิ้น ให้แก่บุคคลทั่วไปได้ใช้เพื่อประโยชน์ส่วนบุคคลหรือ ประโยชน์ทางการศึกษาที่ไม่มีวัตถุประสงค์ในเชิงพาณิชย์ โดยไม่คิดค่าตอบแทนการใช้ ซอฟต์แวร์ ดังนั้น สำนักงานพัฒนาวิทยาศาสตร์และเทคโนโลยีแห่งชาติ จึงไม่มีหน้าที่ใน การดูแล บำรุงรักษา จัดการอบรมการใช้งาน หรือพัฒนาประสิทธิภาพซอฟต์แวร์ รวมทั้ง ไม่รับรองความถูกต้องหรือประสิทธิภาพการทำงานของซอฟต์แวร์ ตลอดจนไม่รับประกัน ความเสียหายต่าง ๆ อันเกิดจากการใชซอฟต์แวร์นี้ทั้งสิ้น''',
-                          ),
-                        ]
-                      ),
-                      style: subDescTextStyleDark(context,
-                        fontWeight: FontWeight.normal),
+                      TextSpan(children: <TextSpan>[
+                        TextSpan(text: '''\u00A0\u00A0\u00A0\u00A0\u00A0'''),
+                        TextSpan(
+                          text:
+                              '''ซอฟต์แวร์นี้เป็นผลงานที่พัฒนาขึ้นโดย นายพชรดนัย กุระกนก นายญาณภัทร ปานเกษม และ นายธัญวรัตม์ ก.วิบูลย์ศักดิ​ศรี จาก มหาวิทยาลัยศรีนครินทรวิโรฒ ภายใต้การดูแลของ นางสาวบรรพตรี คมขำ ภายใต้โครงการ ใบรู้โรค ซึ่งสนับสนุนโดย สำนักงานพัฒนาวิทยาศาสตร์และเทคโนโลยีแห่งชาติ โดยมีวัตถุประสงค์เพื่อส่งเสริมให้นักเรียนและนักศึกษาได้เรียนรู้และฝึกทักษะในการพัฒนา ซอฟต์แวร์ ลิขสิทธิ์ของซอฟต์แวร์นี้จึงเป็นของผู้พัฒนา ซึ่งผู้พัฒนาได้อนุญาติให้สำนักงาน พัฒนาวิทยาศาสตร์และเทคโนโลยีแห่งชาติ เผยแพร่ซอฟต์แวร์นี้ตาม “ต้นฉบับ” โดยไม่มี การแก้ไขดัดแปลงใด ๆ ทั้งสิ้น ให้แก่บุคคลทั่วไปได้ใช้เพื่อประโยชน์ส่วนบุคคลหรือ ประโยชน์ทางการศึกษาที่ไม่มีวัตถุประสงค์ในเชิงพาณิชย์ โดยไม่คิดค่าตอบแทนการใช้ ซอฟต์แวร์ ดังนั้น สำนักงานพัฒนาวิทยาศาสตร์และเทคโนโลยีแห่งชาติ จึงไม่มีหน้าที่ใน การดูแล บำรุงรักษา จัดการอบรมการใช้งาน หรือพัฒนาประสิทธิภาพซอฟต์แวร์ รวมทั้ง ไม่รับรองความถูกต้องหรือประสิทธิภาพการทำงานของซอฟต์แวร์ ตลอดจนไม่รับประกัน ความเสียหายต่าง ๆ อันเกิดจากการใชซอฟต์แวร์นี้ทั้งสิ้น''',
+                        ),
+                      ]),
+                      style:
+                          AppTheme.smallContent(context, color: AppTheme.dark),
                       textAlign: TextAlign.justify,
                       softWrap: true,
                       overflow: TextOverflow.visible,
